860815cc4344d3faf8cf70030c6ca0973a408c24
Alex Mercier <pro.alexandre.mercier@gmail.com>
2014-12-11 13:31:05 -0800

    Added QUnit tests

---
+++ tests_qunit/vanillajs/mocks/store.mock.js
@@ -0,0 +1,87 @@
+/*jshint eqeqeq:false */
+(function (window) {
+  'use strict';
+
+  function StoreMock(name, callback) {
+    callback = callback || function () {};
+    this._data = {
+      todos: []
+    };
+    callback.call(this, this._data);
+  }
+
+  StoreMock.prototype.find = function (query, callback) {
+    if (!callback) {
+      return;
+    }
+
+    var todos = this._data.todos;
+
+    callback.call(this, todos.filter(function (todo) {
+      for (var q in query) {
+        if (query[q] !== todo[q]) {
+          return false;
+        }
+      }
+      return true;
+    }));
+  };
+
+  StoreMock.prototype.findAll = function (callback) {
+    callback = callback || function () {};
+    callback.call(this, this._data.todos);
+  };
+
+  StoreMock.prototype.save = function (updateData, callback, id) {
+    var data = this._data;
+    var todos = data.todos;
+
+    callback = callback || function () {};
+
+    // If an ID was actually given, find the item and update each property
+    if (id) {
+      for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id === id) {
+          for (var key in updateData) {
+            todos[i][key] = updateData[key];
+          }
+          break;
+        }
+      }
+
+      this._data = data;
+      callback.call(this, this._data.todos);
+    } else {
+      // Generate an ID
+      updateData.id = Math.round(new Date().getTime() * Math.random());
+
+      todos.push(updateData);
+      this._data = data;
+      callback.call(this, [updateData]);
+    }
+  };
+
+  StoreMock.prototype.remove = function (id, callback) {
+    var data = this._data;
+    var todos = data.todos;
+
+    for (var i = 0; i < todos.length; i++) {
+      if (todos[i].id == id) {
+        todos.splice(i, 1);
+        break;
+      }
+    }
+
+    this._data = data;
+    callback.call(this, this._data.todos);
+  };
+
+  StoreMock.prototype.drop = function (callback) {
+    this._data = {todos: []};
+    callback.call(this, this._data.todos);
+  };
+
+  // Export to window
+  window.mocks = window.mocks || {};
+  window.mocks.Store = StoreMock;
+})(window);
---
+++ tests_qunit/vanillajs/model.test.js
@@ -0,0 +1,73 @@
+(function() {
+  QUnit.module('Model');
+
+  test('It should add a Model function to the global app object', function() {
+    QUnit.assert.ok(!!app);
+    QUnit.assert.equal(typeof app, 'object');
+    QUnit.assert.ok(!!app.Model);
+    QUnit.assert.equal(typeof app.Model, 'function');
+  });
+
+  test('It should create a new instance of Model', function() {
+    var model = new app.Model();
+    QUnit.assert.ok(model instanceof app.Model);
+  });
+
+  test('It should create a new todo', function () {
+    var store = new mocks.Store('_STORE_');
+    var model = new app.Model(store);
+    QUnit.assert.equal(store._data.todos.length, 0);
+    model.create('_MODEL1_');
+    QUnit.assert.equal(store._data.todos.length, 1);
+    QUnit.assert.equal(store._data.todos[0].title, '_MODEL1_');
+    QUnit.assert.equal(store._data.todos[0].completed, false);
+    QUnit.assert.equal(typeof store._data.todos[0].id, 'number');
+  });
+
+  // read
+  (function() {
+    var store = new mocks.Store('_STORE_');
+    var model = new app.Model(store);
+    model.create('_MODEL1_');
+    model.create('_MODEL1_');
+    model.create('_MODEL2_');
+
+    test('It should read all todos', function() {
+      model.read(function (data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 3);
+      });
+    });
+
+    test('It should read all todos matching a given id', function() {
+      model.read(Number(store._data.todos[0].id), function(data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 1);
+      });
+      model.read(store._data.todos[0].id.toString(), function(data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 1);
+      });
+    });
+
+    test('It should read all todos matching a given filter', function() {
+      model.read({}, function (data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 3);
+      });
+      model.read({ id: store._data.todos[0].id }, function(data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 1);
+      });
+      model.read({ title: '_MODEL1_' }, function(data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 2);
+      });
+      model.read({ id: store._data.todos[0].id, title: '_MODEL1_' }, function(data) {
+        QUnit.assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+        QUnit.assert.equal(data.length, 1);
+      });
+    });
+  })();
+
+})();
