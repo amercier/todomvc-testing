c0d7acd9b5c905d3e4174fec610415b05127c91f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2014-12-11 11:43:30 -0800

    Added more tests

--- tests/vanillajs/model.test.js
+++ tests/vanillajs/model.test.js
@@ -9,10 +9,67 @@
       assert.equal(typeof app.Model, 'function');
     });
 
-    it('Should create a new instance', function() {
+    it('Should create a new instance of Model', function() {
       var model = new app.Model();
       assert.ok(model instanceof app.Model);
     });
+
+    it('Should create a new todo', function () {
+      var store = new mocks.Store('_STORE_');
+      var model = new app.Model(store);
+      assert.equal(store._data.todos.length, 0);
+      model.create('_MODEL1_');
+      assert.equal(store._data.todos.length, 1);
+      assert.equal(store._data.todos[0].title, '_MODEL1_');
+      assert.equal(store._data.todos[0].completed, false);
+      assert.equal(typeof store._data.todos[0].id, 'number');
+    });
+
+    // read
+    (function() {
+      var store = new mocks.Store('_STORE_');
+      var model = new app.Model(store);
+      model.create('_MODEL1_');
+      model.create('_MODEL1_');
+      model.create('_MODEL2_');
+
+      it('Should read all todos', function() {
+        model.read(function (data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 3);
+        });
+      });
+
+      it('Should read all todos matching a given id', function() {
+        model.read(Number(store._data.todos[0].id), function(data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 1);
+        });
+        model.read(store._data.todos[0].id.toString(), function(data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 1);
+        });
+      });
+
+      it('Should read all todos matching a given filter', function() {
+        model.read({}, function (data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 3);
+        });
+        model.read({ id: store._data.todos[0].id }, function(data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 1);
+        });
+        model.read({ title: '_MODEL1_' }, function(data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 2);
+        });
+        model.read({ id: store._data.todos[0].id, title: '_MODEL1_' }, function(data) {
+          assert.equal(Object.prototype.toString.apply(data), '[object Array]');
+          assert.equal(data.length, 1);
+        });
+      });
+    })();
 
   });
 
