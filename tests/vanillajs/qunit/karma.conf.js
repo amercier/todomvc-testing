ebe50a1eda07368a49f444b9562089d68e48e667
Alex Mercier <pro.alexandre.mercier@gmail.com>
2014-12-17 18:46:54 -0800

    Added code coverage analysis for Karma/Jasmine and Karma/QUnit using Istanbul

--- tests/vanillajs/qunit/karma.conf.js
+++ tests/vanillajs/qunit/karma.conf.js
@@ -11,6 +11,19 @@ module.exports = function(config) {
       'app/examples/vanillajs/js/app.js'
     ],
     browsers: ['PhantomJS'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'app/examples/vanillajs/js/*.js': ['coverage']
+    },
+    coverageReporter: {
+      dir: 'tests/vanillajs/qunit/coverage',
+      reporters: [
+        { type: 'html', subdir: 'html' },
+        { type: 'lcovonly', subdir: '.', file: 'report.lcov' },
+        { type: 'text', subdir: '.', file: 'report.txt' },
+        { type: 'text-summary', subdir: '.', file: 'summary.txt' },
+      ]
+    },
     autoWatch: false,
     singleRun: true
   });
