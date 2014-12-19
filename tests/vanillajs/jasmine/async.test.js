0b3fd64e48b1386a3c95b8b1c8fd2470d6c9ef2c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2014-12-19 13:12:08 -0800

    Added async tests for Jasmine

---
+++ tests/vanillajs/jasmine/async.test.js
@@ -0,0 +1,66 @@
+(function() {
+
+  var foo1 = false, foo2 = false, nestedFoo = false;
+
+  describe('Async', function() {
+
+    beforeEach(function (done) {
+      var doneCount = 0;
+
+      setTimeout(function() {
+        foo1 = true;
+        setTimeout(function() {
+          nestedFoo = true;
+          ++doneCount == 2 && done();
+        }, 50);
+      }, 50);
+      setTimeout(function() {
+        foo2 = true;
+        ++doneCount == 2 && done();
+      }, 150);
+    });
+
+    it('Should wait for beforeEach to terminate', function() {
+      expect(foo1).toEqual(true);
+      expect(foo2).toEqual(true);
+      expect(nestedFoo).toEqual(true);
+    });
+
+    it('Should support asynchronous tests', function(done) {
+      setTimeout(function() {
+        expect(true).toEqual(true);
+        done();
+      }, 100);
+    });
+
+    it('Should support multiple asynchronous tests', function(done) {
+      var doneCount = 0;
+
+      setTimeout(function() {
+        expect(true).toEqual(true);
+        ++doneCount == 2 && done();
+      }, 200);
+
+      setTimeout(function() {
+        expect(true).toEqual(true);
+        ++doneCount == 2 && done();
+      }, 100);
+    });
+
+    it('Should support nested asynchronous tests', function(done) {
+      var doneCount = 0;
+      setTimeout(function() {
+        expect(true).toEqual(true);
+
+        setTimeout(function() {
+          expect(true).toEqual(true);
+          ++doneCount == 2 && done();
+        }, 100);
+
+        ++doneCount == 2 && done();
+      }, 100);
+    });
+
+  });
+
+})();
