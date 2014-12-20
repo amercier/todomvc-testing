26d5996ca3b771f44758798a942722fa3504fa77
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-12-19 22:23:13 -0800

    Fixed Blanket configuration

--- tests/vanillajs/qunit/testem.js
+++ tests/vanillajs/qunit/testem.js
@@ -1,22 +1,37 @@
 /*jshint browser:false, node:true */
-module.exports = {
-  'framework': 'qunit',
-  'test_page': 'tests/vanillajs/qunit/index.mustache',
-  'launch_in_ci': [
+
+var globby = require('globby');
+
+var config = {
+  framework: 'qunit',
+  test_page: 'tests/vanillajs/qunit/index.mustache?coverage',
+  launch_in_ci: [
     'PhantomJS'
   ],
-  'launch_in_dev': [
+  launch_in_dev: [
     'PhantomJS'
   ],
-  'src_files': [
+  css_files: [
+    'test.css'
+  ],
+  _lib_files: globby.sync([
+    'bower_components/rsvp/rsvp.js',
+    'bower_components/blanket/dist/qunit/blanket.js'
+  ]),
+  _src_files: globby.sync([
     'app/examples/vanillajs/js/*.js',
+    '!app/examples/vanillajs/js/app.js'
+  ]),
+  _test_files: globby.sync([
     'tests/vanillajs/mocks/*.mock.js',
     'tests/vanillajs/qunit/*.test.js'
-  ],
-  'src_files_ignore': [
-    'app/examples/vanillajs/js/app.js'
-  ],
-  'watch_files': [
-    'tests/vanillajs/qunit/index.mustache'
-  ]
+  ])
 };
+
+config.src_files = [].join(
+  config._src_files,
+  config._lib_files,
+  config._test_files
+);
+
+module.exports = config;
