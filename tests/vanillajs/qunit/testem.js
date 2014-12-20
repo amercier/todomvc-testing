26d9f7666a7974b8c438db4e89d8651ccfeac692
Alex Mercier <pro.alexandre.mercier@gmail.com>
2014-12-19 18:20:37 -0800

    WIP

--- tests/vanillajs/qunit/testem.js
+++ tests/vanillajs/qunit/testem.js
@@ -1,22 +1,26 @@
 /*jshint browser:false, node:true */
 module.exports = {
-  'framework': 'qunit',
-  'test_page': 'tests/vanillajs/qunit/index.mustache',
-  'launch_in_ci': [
+  framework: 'qunit',
+  test_page: 'tests/vanillajs/qunit/index.mustache',
+  launch_in_ci: [
     'PhantomJS'
   ],
-  'launch_in_dev': [
+  launch_in_dev: [
     'PhantomJS'
   ],
-  'src_files': [
+  src_files: [
     'app/examples/vanillajs/js/*.js',
     'tests/vanillajs/mocks/*.mock.js',
     'tests/vanillajs/qunit/*.test.js'
   ],
-  'src_files_ignore': [
+  src_files_ignore: [
     'app/examples/vanillajs/js/app.js'
   ],
-  'watch_files': [
+  watch_files: [
     'tests/vanillajs/qunit/index.mustache'
-  ]
+  ],
+  lib_files: [
+    { src:'bower_components/blanket/dist/qunit/blanket.js', attr:'data-cover-only="../../../app/.*"' }
+  ],
+  src_attr: 'data-cover'
 };
